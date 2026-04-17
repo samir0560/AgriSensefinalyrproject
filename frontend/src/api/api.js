26259a6e3fdbced143c45a8c5bd453fcfe800101
Samir Chaudhary <samirchaudhary11145@gmail.com>
2026-04-17 13:52:24 +0545

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://agrisensefinalyrprojectt.onrender.com/';
 
 const authHeaders = (token) => (token ? { Authorization: `Bearer ${token}` } : {});
 
